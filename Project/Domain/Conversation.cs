7af0efe005c6d274b3882248dbdcf0efee69e800
Aleksy Ruszala <16pxdesign@gmail.com>
2019-10-04 04:15:56 +0100

    Create early domain models

---
+++ Project/Domain/Conversation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Conversation
+    {
+        public Guid Id { get; set; }
+        public List<Message> Messages { get; set; }
+        public List<User> User { get; set; }
+
+
+    }
+}
