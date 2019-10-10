f3487badbed2c459fcb01cb379e68a04dbd7bf3d
Aleksy Ruszala <16pxdesign@gmail.com>
2019-10-10 20:03:54 +0100

    WebApi success connection established between Server <-> Client (tested)

--- Project/Presenter/Helpers/ServerConnection.cs
+++ Project/Presenter/Helpers/ServerConnection.cs
@@ -10,24 +10,38 @@ namespace Presenter.Helpers
 {
     public class ServerConnection : IServerConnection
     {
-        private const string HttpURL = "http://localhost:8080/chat";
+        private const string HttpUrLforSingalR = "http://localhost:8080/chat";
+        private const string HttpUrLforWebApi = "http://localhost:8080/api/login";
         private HubConnection ServerChatConnection { get; set; }
         private IHubProxy ServerChat { get; set; }
 
         public ServerConnection()
         {
+            //ConnectAsync();
+            WebApiConnect();
+        }
 
-            ServerChatConnection = new HubConnection(HttpURL);
-            ServerChat = ServerChatConnection.CreateHubProxy("ChatHub");
-            ConnectAsync();
-            Console.WriteLine("Server conn alive");
-            
-
-
+        private void WebApiConnect()
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage message = httpClient.GetAsync(HttpUrLforWebApi).Result;
+ 
+            if (message.IsSuccessStatusCode)
+            {
+                var messaResult = message.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(messaResult);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
         }
 
         private async void ConnectAsync()
         {
+            ServerChatConnection = new HubConnection(HttpUrLforSingalR);
+            ServerChat = ServerChatConnection.CreateHubProxy("ChatHub");
+
             try
             {
                 await ServerChatConnection.Start();
@@ -36,16 +50,12 @@ namespace Presenter.Helpers
             {
                 Console.WriteLine("Not connected");
             }
+
+            Console.WriteLine("Server conn alive");
         }
-
-      
-
     }
 
     public interface IServerConnection
     {
-
     }
-
-
 }
