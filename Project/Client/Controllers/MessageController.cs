d423f5e2eb82208d843197b2fd822d9e3a975648
Aleksy Ruszala <16pxdesign@gmail.com>
2019-10-18 23:01:54 +0100

    Create Message controller and view and test lifecycle for unity

---
+++ Project/Client/Controllers/MessageController.cs
@@ -0,0 +1,7 @@
+﻿namespace Client.Controllers
+{
+    public class MessageController
+    {
+        
+    }
+}
---
+++ Project/Client/Views/MessageView.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Client.Views
+{
+    public partial class MessageView : Form
+    {
+        public MessageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
